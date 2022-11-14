d75142d71a0f2f3433fd862c8661d66777d9ce17
amitX102 <118204528+amitX102@users.noreply.github.com>
2022-11-14 22:21:14 +0530

    Java Default Constructor
    Java Default Constructor

---
+++ Light.java
@@ -0,0 +1,16 @@
+class Greenlight{
+        int a;
+		boolean b;
+		 String l;
+		}
+	
+	
+class Light{
+     public static void main (String args[]){
+	        Greenlight m=new Greenlight();
+			
+			System.out.println("integer "+ m.a);
+			System.out.println("boolen "+ m.b);
+			System.out.println("string "+ m.l);
+			}
+	}

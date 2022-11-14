5e26e25c5c20d56c12e25d1f98914efce0c92fca
amitX102 <118204528+amitX102@users.noreply.github.com>
2022-11-14 22:18:34 +0530

    Default Explicit Constructor
    Default Explicit Constructor in java

---
+++ Lights.java
@@ -0,0 +1,29 @@
+class Greenlite{
+      int a;
+	  boolean b;
+	  String c;
+	  
+	  Greenlite()
+	  {   
+	      a=10;
+		  b=true;
+		  c="amit";
+	  }
+}
+
+
+class Lights{
+	  public static void main (String args[]){
+		      Greenlite l1= new Greenlite();
+			   Greenlite l2= new Greenlite();
+			    Greenlite l3= new Greenlite();
+				
+				System.out.println("integar"+ l1.a);
+				System.out.println("integar"+ l2.b);
+				System.out.println("integar"+ l3.c);
+				
+				
+				
+	  }
+	   
+}
---
+++ Lightx.java
@@ -0,0 +1,28 @@
+class Greenlight{
+      int a;
+	  boolean b;
+	  String c;
+	  
+	  
+	  Greenlight(int i, boolean j, String k)
+	  {
+		  a=i;
+		  b=j;
+		  c=k;
+	  }
+}
+
+
+class Lightx{
+	     public static void main (String args[]){
+			     Greenlight l1=new Greenlight(14,true , "ak");
+				   Greenlight l2=new Greenlight(134,true , "aks");
+				   Greenlight l3=new Greenlight(15,true , "sak");
+				   
+				   
+				   
+				  // System.out.println("integar"+ l1.a);
+				   System.out.println("integar"+ l2.b);
+				//System.out.println("integar"+ l3.c);
+		 }
+}

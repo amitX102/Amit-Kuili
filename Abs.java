257682586cc30b29734d0acf4691d0af937bae67
amitX102 <118204528+amitX102@users.noreply.github.com>
2022-11-17 22:43:32 +0530

    Add files via upload

---
+++ Abs.java
@@ -0,0 +1,27 @@
+//acces modiferar 2->>abstract 
+
+
+abstract class Lt{
+	  public void swin(){
+		      boolean a=true;
+	  }
+	  abstract public double price();
+}
+
+class Lps extends Lt{
+	    double t;
+	    public double price(){
+			  t=4.5;
+			  return (t);
+		}
+}
+class Abs{
+	  public static void main(String args[]){
+		    Lps ps=new Lps();
+			ps.price();
+			ps.swin();
+			
+			System.out.println("The valu of t is "+ ps.t);
+			System.out.println("The valu of t is "+ ps.swin);
+	  }
+}

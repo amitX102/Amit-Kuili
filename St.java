bc6cff450f14d45117ee2c429cc5d343ac959075
amitX102 <118204528+amitX102@users.noreply.github.com>
2022-11-17 22:42:50 +0530

    Add files via upload

---
+++ St.java
@@ -0,0 +1,25 @@
+class Stc{
+	  public static void main(String args[]){
+	  
+	  
+	      St.mul(20,30);
+		  St.div(30,20);
+		 System.out.println("div is"+ St.m);
+		  System.out.println("div is"+ St.n);
+	  
+	  }
+}
+
+class St{
+	 
+	       static int m,n;
+		   static int mul(int x, int y){
+			      m=x+y;
+				  return m;
+		   }
+		   
+		   static int div(int x, int y){
+			      n=x/y;
+				  return n;
+		   }
+}
